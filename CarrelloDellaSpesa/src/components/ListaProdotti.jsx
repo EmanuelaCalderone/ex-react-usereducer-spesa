6edbfa88bd85f4e22ab75fd0ea9f9ac5f6f19742
Emanuela Calderone <emanuelacalderone3006@gmail.com>
2025-07-02 14:59:22 +0200

    complete milestone 3

--- CarrelloDellaSpesa/src/components/ListaProdotti.jsx
+++ CarrelloDellaSpesa/src/components/ListaProdotti.jsx
@@ -14,7 +14,30 @@ function ListaProdotti() {
         const isIncluded = addedProducts.some(p => p.name === product.name);
         if (!isIncluded) {
             setAddedProducts([...addedProducts, { ...product, quantity: 1 }]);
+        } else {
+            updateProductQuantity(product);
         }
+    }
+
+    function updateProductQuantity(product) {
+        const updatedCart = addedProducts.map(p => {
+            if (p.name === product.name) {
+                return { ...p, quantity: p.quantity + 1 }
+            }
+            return p;
+        });
+        setAddedProducts(updatedCart);
+    }
+
+    function removeFromCart(product) {
+        const updatedCart = addedProducts.map(p => {
+            if (p.name === product.name) {
+                return { ...p, quantity: p.quantity - 1 };
+            }
+            return p;
+        })
+            .filter(p => p.quantity > 0);
+        setAddedProducts(updatedCart);
     }
 
     const elencoProdotti = products.map((product, index) => (
@@ -22,29 +45,41 @@ function ListaProdotti() {
             {product.name} - € {product.price}
             <div>
                 <button onClick={() => addToCart(product)}>Aggiungi al carrello</button>
+                <button onClick={() => removeFromCart(product)}>Rimuovi</button>
+
             </div>
         </li>
     ));
 
-    return (
-        <div>
-            <ul>
-                {elencoProdotti}
-            </ul>
+    const total = addedProducts.reduce((acc, product) => {
+        return acc + product.price * product.quantity;
+    }, 0);
 
-            {addedProducts.length > 0 && (
-                <div>
-                    <h2>Il tuo carrello</h2>
-                    <ul>
-                        {addedProducts.map((p, index) => (
-                            <li key={index}>
-                                {p.name} - € {p.price} - quantità: {p.quantity}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            )}
-        </div>
+    return (
+        <>
+            <div>
+                <ul>
+                    {elencoProdotti}
+                </ul>
+
+                {addedProducts.length > 0 && (
+                    <div>
+                        <h2>Il tuo carrello</h2>
+                        <ul>
+                            {addedProducts.map((p, index) => (
+                                <li key={index}>
+                                    {p.name} - € {p.price} - quantità: {p.quantity}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
+            </div>
+            <div>
+                <h2>Totale</h2>
+                € {total.toFixed(2)}
+            </div>
+        </>
     );
 }
 

2f5c984ea34524e51553ca9918ef88f9c7215afb
Emanuela Calderone <emanuelacalderone3006@gmail.com>
2025-07-02 14:36:34 +0200

    complete milestone 2

--- CarrelloDellaSpesa/src/components/ListaProdotti.jsx
+++ CarrelloDellaSpesa/src/components/ListaProdotti.jsx
@@ -1,5 +1,4 @@
-import react from 'react';
-
+import React, { useState } from 'react';
 
 const products = [
     { name: 'Mela', price: 0.5 },
@@ -8,12 +7,22 @@ const products = [
     { name: 'Pasta', price: 0.7 },
 ];
 
-
 function ListaProdotti() {
+    const [addedProducts, setAddedProducts] = useState([]);
 
-    const elencoProdotti = products.map((prodotto, index) => (
+    function addToCart(product) {
+        const isIncluded = addedProducts.some(p => p.name === product.name);
+        if (!isIncluded) {
+            setAddedProducts([...addedProducts, { ...product, quantity: 1 }]);
+        }
+    }
+
+    const elencoProdotti = products.map((product, index) => (
         <li key={index}>
-            {prodotto.name} - € {prodotto.price}
+            {product.name} - € {product.price}
+            <div>
+                <button onClick={() => addToCart(product)}>Aggiungi al carrello</button>
+            </div>
         </li>
     ));
 
@@ -22,10 +31,21 @@ function ListaProdotti() {
             <ul>
                 {elencoProdotti}
             </ul>
+
+            {addedProducts.length > 0 && (
+                <div>
+                    <h2>Il tuo carrello</h2>
+                    <ul>
+                        {addedProducts.map((p, index) => (
+                            <li key={index}>
+                                {p.name} - € {p.price} - quantità: {p.quantity}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </div>
-    )
-
-
+    );
 }
 
 export default ListaProdotti;
